598a692b56b3d2361201f8385b32ecec4e4888df
venbs <eeven66@gmail.com>
2020-06-20 22:59:00 +0800

    v

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ let smooth_dir;
 let ghost = 2
 
 function setup(){
-    Canvas0 = createCanvas(screen.width, screen.height);
+    Canvas0 = createCanvas(980, 980);
     Canvas0.mousePressed(userStartAudio());
     colorMode(HSB);
     frameRate(FRAMERATE);
